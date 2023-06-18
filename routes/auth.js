dcc9dea6b242452caa4be40ac7b8fbc49765a50a
Jay Duong <jayvin.duong@gmail.com>
2023-06-17 21:48:41 -0400

    chore: fixing the steam authenication on the service end

--- routes/auth.js
+++ routes/auth.js
@@ -13,7 +13,7 @@ require('dotenv').config()
 
 const steam = new SteamAuth({
   realm: process.env.HOST_BACKEND,
-  returnUrl: `${process.env.HOST_BACKEND}/auth/steam/authenticate?`,
+  returnUrl: `${process.env.HOST_BACKEND}/auth/steam/authenticate`,
   apiKey: process.env.STEAM_API_KEY,
 })
 
@@ -22,16 +22,13 @@ router
     res.send('AUTH')
   })
   .get('/steam', async (req, res) => {
-    const response = await steam.getRedirectUrl()
-
-    res.status(200).json({
-      success: true,
-      URL: response,
-    })
+    const redirectUrl = await steam.getRedirectUrl()
+    res.redirect(redirectUrl)
   })
   .get('/steam/authenticate', async (req, res) => {
     try {
       const response = await steam.authenticate(req)
+      res.send(response)
     } catch (error) {
       res.status(404).json({
         success: false,
